9635dc1e672fafc77e2bf70370b398776c3f6b70
Tibi Lapos <tibilapos@Tibis-MacBook-Pro.local>
2023-04-02 11:58:09 +0200

    some changes in design

--- src/Page/Home/Menu.tsx
+++ src/Page/Home/Menu.tsx
@@ -1,8 +1,6 @@
 import { BorderColor } from '@mui/icons-material';
 import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
-
-
-
+import '@fontsource/roboto';
 
 
 const styles = {
@@ -13,30 +11,35 @@ const styles = {
       },
       logo: {
         textAlign: 'center',
-        fontFamily: 'Montserrat, sans-serif',
-        fontWeight: 700,
-        fontSize: '1.5rem',
+        fontFamily: 'Roboto, sans-serif',
         textTransform: 'uppercase',
-        letterSpacing: '0.2rem',
         color: '#fff',
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
       },
+      logo_letter:{
+        fontWeight: 400,
+        fontSize: '1.5rem',
+        letterSpacing: '0.2rem',
+        marginLeft:5,
+        },
       firstLetter: {
+        fontFamily: 'Montserrat, sans-serif',
+        fontWeight: 800,
         color: '#f44336',
         fontSize: '2.5rem',
       },
       firstLetterShadow: {
-        textShadow: '4px -4px 1px #7B032C',
+        textShadow: '5px -2px 1px #7B032C',
       },
       appBar: {
         backgroundColor: '#081b27',
       },
       button: {
         color: '#fff',
-        fontWeight: 'bold',
         margin: '0 0px',
+        fontWeight: 600,
         fontFamily: 'Montserrat, sans-serif',
         textTransform: 'uppercase',
         letterSpacing: '0.1rem',
@@ -58,7 +61,7 @@ export default function Menu() {
                 <span style={styles.firstLetter}>
                     <span style={styles.firstLetterShadow}>M</span>
                 </span>
-                <span>ovie Website</span>
+                <span style={styles.logo_letter}>ovie Website</span>
           </Typography>
           <Box sx={styles.button_box}>
             <Button color="inherit" sx={styles.button}>
